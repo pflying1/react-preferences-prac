e31d77bccfb11ddf2320d5c15c9afd1fb5549787
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-07 17:30:17 +0900

    타입스크립트 환경설정 express+타입스크립트 오류 해결 과정

---
+++ build/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './test.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>시작</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './build/src/index.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(tsx?|js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+const serverConfig = {
+  entry : 
+}
